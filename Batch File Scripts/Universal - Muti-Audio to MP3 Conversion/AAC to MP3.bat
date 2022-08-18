75ab9d4e73ec42774722082d969fc2ac1db7888f
DevShaw <sys.markshaw@gmail.com>
2022-08-18 11:06:39 -0400

    Delete Batch File Scripts directory

--- Batch File Scripts/Universal - Muti-Audio to MP3 Conversion/AAC to MP3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem
-if not exist "Converted\" MD "Converted"
-FOR %%A IN ("*.aac") Do ffmpeg -i "%%~nA.aac" -c:v copy -map_metadata 0 -id3v2_version 3 -b:a 320k "Converted/%%~nA.mp3"
-pause
